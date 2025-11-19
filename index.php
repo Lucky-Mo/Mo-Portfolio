4f279e86eec44eb42c7575abab72c96fa9cf99af
Its_Mootje <mdothman06@outlook.com>
2025-11-19 11:13:10 +0100

    New start on Mo-Portfolio
    Started again on Mo-Portfolio:
    made the files,
    nested the files,
    made a start on the files.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Mo Site</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>body{margin:0}</style>
+</head>
+<body>
+
+    <header>
+    <nav class="bg-black p-4 text-red-500 flex justify-between items-center">
+        <div class="text-2xl font-bold">
+            Mo Site
+        </div>
+        <div>
+            <a href="index.html" class="mx-2 hover:text-white">Home</a>
+            <a href="Mo.html" class="mx-2 hover:text-white">Contact</a>
+        </div>
+    </header>
+
+    <main>
+        <section class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+            <h1 class="text-4xl font-bold">Dit is onze Portfolio MoLuSeMi</h1>
+        </section>
+    </main>
+
+    <footer>
+
+    </footer>
+   
+</body>
+</html>
