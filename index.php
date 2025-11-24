cee66e7fefe8ae7d6503fca43756d7d71095b8ab
Its_Mootje <mdothman06@outlook.com>
2025-11-24 17:47:53 +0100

    24/11

--- index.php
+++ index.php
@@ -6,31 +6,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Mo-Portfolio</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
     <meta http-equiv="refresh" content=""> 
  </head>
     <body>
         <header>
             <a class="logo" href="index.php"><img src="images/favicon.ico" alt="logo"></a>
             <nav>
-                <ul class="nav__links">
-                    <li><a href="mo.php">About Me</a></li>
-                    <li><a href="projects.php">Projects</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
+                <!-- <div class="nav">
+                    <a href="mo.php">Home</a>
+                </div>  -->
+                   <a href="index.php">About Me</a>
+                   <a href="projects.php">My Projects</a>
+                   <a href="contact.php">Contact</a>
             </nav>
-            <a class="cta" href="#">Translate</a>
-            <p class="menu cta">Menu</p>
+            
+            
         </header>
-        <div class="overlay">
-            <a class="close">&times;</a>
-            <div class="overlay__content">
-                <a href="#">Services</a>
-                <a href="#">Projects</a>
-                <a href="#">About</a>
-            </div>
-        </div>
-        <script type="text/javascript" src="mobile.js"></script>
     </body>
 </html>
 
