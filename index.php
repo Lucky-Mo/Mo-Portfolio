18a26882e7ac6d0fdac51dcbde08364beb0cc968
Its_Mootje <mdothman06@outlook.com>
2025-11-25 14:29:05 +0100

    laptop 25/11

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <title>Welcome to Mo-Portfolio</title>
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
     <meta http-equiv="refresh" content=""> 
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
  </head>
     <body>
         <nav class="navbar">
@@ -19,11 +20,11 @@
                
                 <!-- Nav right (links) -->
                 <ul class="nav-menu" id="nav-menu">
-                    <li class="nav-item"><a href="index.php" class="nav-middel">Home</a></li>
-                    <li class="nav-item"><a href="aboutme.php" class="nav-middel">About Me</a></li>
-                    <li class="nav-item"><a href="service.php" class="nav-middel">Services</a></li>
-                    <li class="nav-item"><a href="projects.php" class="nav-middel">Projects</a></li>
-                    <li class="nav-item"><a href="contact.php" class="nav-middel">Contact</a></li>
+                    <li class="nav-item, container"><a href="index.php" class="nav-middel">Home</a></li>
+                    <li class="nav-item, container"><a href="aboutme.php" class="nav-middel">About Me</a></li>
+                    <li class="nav-item, container"><a href="service.php" class="nav-middel">Services</a></li>
+                    <li class="nav-item, container"><a href="projects.php" class="nav-middel">Projects</a></li>
+                    <li class="nav-item, container"><a href="contact.php" class="nav-middel">Contact</a></li>
 
                     <!-- ????????!!!!!!! -->
                     <div class="nav-right">
@@ -69,7 +70,7 @@
             </div>
         </nav>
 
-        <main>
+        <main class="grid grid-cols-4 gap-4">
             <!-- From Uiverse.io by D3OXY --> 
             <div class="card" id="cardid1">
                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path></svg>
